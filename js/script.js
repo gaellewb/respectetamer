44bd2cf6a6311d1f79a63163e804ca1f23bece89
Gaelle Walczyna <gaellewalczyna@gmail.com>
2023-03-31 15:17:25 +0200

    apparition texte accueil

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('connecté');
+// Test pour voir si la page JS est bien appelée
+// console.log('connecté');
+
+window.addEventListener('scroll', apparationTexte)
+    // console.log('defilement détécté');
+
+let appear = document.querySelector('.appear');
+let hauteur;
+
+    function apparationTexte(){
+        hauteur = document.documentElement.scrollTop;
+        console.log(hauteur, 'nombre de pixels depuis le haut de la page');
+
+        if (hauteur > 280) {
+            // console.log('affichage du texte');
+            appear.classList.add('appear-visible');
+        }
+
+    }
