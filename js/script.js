b7fb5e4fb951e55fd3301341e3357f72f40b3d88
Gaelle Walczyna <gaellewalczyna@gmail.com>
2023-04-14 17:13:11 +0200

    nettoyage

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // Test pour voir si la page JS est bien appelée
 // console.log('connecté');
 
-// PAGE INDEX ----------------------------------
 
+// PAGE INDEX ----------------------------------
 // Apparition d'un texte au scroll
-window.addEventListener('scroll', apparationTexte)
+window.addEventListener('scroll', apparitionTexte)
     // console.log('defilement détécté');
 
 let appear = document.querySelector('.appear');
 let appear2 = document.querySelector('.appear2');
-// let exist = oElementHTML.classList.contains('appear');
 let hauteur;
 
-    function apparationTexte(){
+    function apparitionTexte(){
         hauteur = document.documentElement.scrollTop;
         // console.log(hauteur, 'nombre de pixels depuis le haut de la page');
 
-        if (document.querySelector('.appear')){
+// Boucles pour vérifier si les classes appear et appear2 sont présentes dans la page :
+        if (document.querySelector('.appear')){ 
             if (hauteur > 280) {
                 // console.log('affichage du texte');
                 appear.classList.add('appear-visible');
@@ -33,18 +33,18 @@ let hauteur;
 
 
 // PAGE AGENDA -----------------------------------
-
-// Fonction pour mettre la première lettre en majuscule
+// Fonction pour mettre la première lettre en majuscule, elle sert pour l'API mais je l'ai mise ici car elle peut être utile ailleurs
 function capitalize(str){
     return str[0].toUpperCase() + str.slice(1);
 };
+
 
 
 // PAGE FAQ -------------------------------------
 const questions = document.querySelectorAll('.question');
 // console.log(questions);
 
-// => Façon simplifiée pour écrir les fonctions anonymes, les fonctions fléchées sont des fonctions sans le mot clé function
+// => Façon simplifiée pour écrire les fonctions anonymes, les fonctions fléchées sont des fonctions sans le mot clé function
 questions.forEach((item) => {
     // console.log('item');
 // Chaque élément du tableau est représenté par item
@@ -54,7 +54,7 @@ questions.forEach((item) => {
 // je sélectionne et stocke la DIV suivante de l'item cliqué
         const next = item.nextElementSibling;
         // console.log(next);
-// Je rend visible ou invisible la réponse
+// Je rends visible ou invisible la réponse
         next.classList.toggle('reponse-visible');
 // Je cible et stocke l'icône de l'item
         icone = item.lastElementChild;
